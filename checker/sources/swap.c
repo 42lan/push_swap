1377e4fb3a21eadfb8623fcfae3552386b00b76b
4slan <amalsago@student.42.fr>
2020-02-22 17:56:40 +0100

    improve: create swap_top2el() and calling it from sa/sb

--- checker/sources/swap.c
+++ checker/sources/swap.c
@@ -6,34 +6,41 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/22 01:52:32 by amalsago          #+#    #+#             */
-/*   Updated: 2020/02/22 14:05:03 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/02/22 17:53:48 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "checker.h"
 
-void			sa(void)
+/*
+** swap_top2el() - swaps two first elements of given array
+*/
+
+static void		swap_top2el(int *stack, int len)
 {
 	int			*a;
 	int			*b;
 
-	if (get_stack()->index_a > 0)
+	if (len > 0)
 	{
-		a = &(get_stack()->stack_a[0]);
-		b = &(get_stack()->stack_a[1]);
+		a = &(stack[0]);
+		b = &(stack[1]);
 		ft_swap_xor(a, b);
 	}
 }
 
+void			sa(void)
+{
+	swap_top2el(get_stack()->stack_a, get_stack()->len_a);
+}
+
 void			sb(void)
 {
-	int			*a;
-	int			*b;
+	swap_top2el(get_stack()->stack_b, get_stack()->len_b);
+}
 
-	if (get_stack()->index_b > 0)
-	{
-		a = &(get_stack()->stack_b[0]);
-		b = &(get_stack()->stack_b[1]);
-		ft_swap_xor(a, b);
-	}
+void			ss(void)
+{
+	sa();
+	sb();
 }
