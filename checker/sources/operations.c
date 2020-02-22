32f9b28520ec680ae0d15dfbe4704d3829a29922
4slan <amalsago@student.42.fr>
2020-02-22 17:53:04 +0100

    feat: add ternary operation of operations

--- checker/sources/operations.c
+++ checker/sources/operations.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/22 13:57:15 by amalsago          #+#    #+#             */
-/*   Updated: 2020/02/22 13:58:01 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/02/22 14:58:28 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,15 +25,15 @@ int			valid_operation(char *op)
 void		execute_operation(char *op)
 {
 	ft_strequ(op, "sa") ? sa(): 0;
+	ft_strequ(op, "sb") ? sb(): 0;
+	ft_strequ(op, "ss") ? ss(): 0;
+	ft_strequ(op, "ra") ? ra(): 0;
+	ft_strequ(op, "rb") ? rb(): 0;
+	ft_strequ(op, "rr") ? rr(): 0;
+	ft_strequ(op, "rra") ? rra(): 0;
+	ft_strequ(op, "rrb") ? rrb(): 0;
+	ft_strequ(op, "rrr") ? rrr(): 0;
 	/*
-	ft_strequ(op, "sb")
-	ft_strequ(op, "ss")
-	ft_strequ(op, "ra")
-	ft_strequ(op, "rb")
-	ft_strequ(op, "rr")
-	ft_strequ(op, "rra")
-	ft_strequ(op, "rrb")
-	ft_strequ(op, "rrr")
 	ft_strequ(op, "pa")
 	ft_strequ(op, "pb")
 	*/
