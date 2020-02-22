5fe32d081275c6d229a7d1d09ffcbd6640a738aa
4slan <amalsago@student.42.fr>
2020-02-22 14:07:22 +0100

    feat: add prototypes of operations and tools

--- checker/includes/checker.h
+++ checker/includes/checker.h
@@ -20,6 +20,10 @@
 # include "get_next_line.h"
 # include "system_libraries.h"
 
+/*
+** STACKS
+*/
+
 typedef struct	s_stack
 {
 	int			*stack_a;
@@ -32,14 +36,31 @@ typedef struct	s_stack
 void			initialize_stack(char **av);
 t_stack			*get_stack(void);
 
+/*
+** PARSING
+*/
+
 void			parse_args(int ac, char ***av);
 void			check_ints(char **av);
 void			check_dups(char **av);
 void			check_lims(char **av);
 
-void			sa(void);
+/*
+** OPERATIONS
+*/
 
-void	p(char **av);
+int			valid_operation(char *op);
+void		execute_operation(char *op);
+void		sa(void);
+void		sb(void);
+
+/*
+** TOOLS
+*/
+
+void	print_av(char **av);
+void	print_stack(int *stack, int size);
+
 /*
 ** CHECKER_H
 */
