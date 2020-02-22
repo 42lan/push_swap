a55adf1dd689a8c6963fd37dccc85a740fe779d9
4slan <amalsago@student.42.fr>
2020-02-22 17:57:38 +0100

    improve: print empty slot if stack is empty

--- checker/sources/tools.c
+++ checker/sources/tools.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/22 13:58:16 by amalsago          #+#    #+#             */
-/*   Updated: 2020/02/22 14:01:53 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/02/22 14:23:02 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,24 @@ void	print_av(char **av)
 {
 	int i = -1;
 	while (av[++i])
-		ft_printf("[%s]\n", av[i]);
+	{
+		if (av[i])
+			ft_printf("[%s]", av[i]);
+		else
+			ft_printf("[ ]");
+	}
+	ft_printf("\n");
 }
 
 void print_stack(int *stack, int size)
 {
 	int j = -1;
 	while (++j <= size)
-		ft_printf("[%d]\n", stack[j]);
+	{
+		if (stack[j])
+			ft_printf("[%d]", stack[j]);
+		else
+			ft_printf("[ ]");
+	}
+	ft_printf("\n");
 }
