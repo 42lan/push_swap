daae5e352e5060971a15cef8d59764cac3804933
4slan <amalsago@student.42.fr>
2020-02-23 14:19:21 +0100

    feat: add call to pa() and pb()

--- checker/sources/operations.c
+++ checker/sources/operations.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/22 13:57:15 by amalsago          #+#    #+#             */
-/*   Updated: 2020/02/22 14:58:28 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/02/22 18:59:09 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 
 int			valid_operation(char *op)
 {
-	if (ft_strequ(op, "sa") || ft_strequ(op, "sb") || ft_strequ(op, "ss")
+	if (ft_strequ(op, "pa") || ft_strequ(op, "pb")
+		|| ft_strequ(op, "sa") || ft_strequ(op, "sb") || ft_strequ(op, "ss")
 		|| ft_strequ(op, "ra") || ft_strequ(op, "rb") ||  ft_strequ(op, "rr")
 		|| ft_strequ(op, "rra") || ft_strequ(op, "rrb") || ft_strequ(op, "rrr")
 		|| ft_strequ(op, "pa") || ft_strequ(op, "pb"))
@@ -24,6 +25,8 @@ int			valid_operation(char *op)
 
 void		execute_operation(char *op)
 {
+	ft_strequ(op, "pa") ? pa(): 0;
+	ft_strequ(op, "pb") ? pb(): 0;
 	ft_strequ(op, "sa") ? sa(): 0;
 	ft_strequ(op, "sb") ? sb(): 0;
 	ft_strequ(op, "ss") ? ss(): 0;
@@ -33,8 +36,4 @@ void		execute_operation(char *op)
 	ft_strequ(op, "rra") ? rra(): 0;
 	ft_strequ(op, "rrb") ? rrb(): 0;
 	ft_strequ(op, "rrr") ? rrr(): 0;
-	/*
-	ft_strequ(op, "pa")
-	ft_strequ(op, "pb")
-	*/
 }
