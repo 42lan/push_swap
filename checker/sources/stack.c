22d482c17488e30ff3de56e8d7aab867c2b02f84
4slan <amalsago@student.42.fr>
2020-02-23 15:46:57 +0100

    style: use variable to stay in norm

--- checker/sources/stack.c
+++ checker/sources/stack.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/22 01:59:39 by amalsago          #+#    #+#             */
-/*   Updated: 2020/02/23 11:51:18 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/02/23 15:41:05 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,11 @@ t_stack				*get_stack(void)
 
 void				initialize_stack(char **av)
 {
+	int		top_index;
+
+	top_index = ft_arraysize(av) - 1;
 	get_stack()->stack_a = ft_ca2ia(av);
-	get_stack()->top_index_a = ft_arraysize(av) - 1;
-	get_stack()->stack_b = (int *)ft_memalloc(sizeof(int) * get_stack()->len_a);
+	get_stack()->top_index_a = top_index;
+	get_stack()->stack_b = (int *)ft_memalloc(sizeof(int) * top_index);
 	get_stack()->top_index_b = -1;
 }
