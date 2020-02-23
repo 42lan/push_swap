fa290699bd80a1f0cafc1ecce81356fde8d1a410
4slan <amalsago@student.42.fr>
2020-02-23 14:23:50 +0100

    improve: correct behaviour of reverse rotate function

--- checker/sources/reverse_rotate.c
+++ checker/sources/reverse_rotate.c
@@ -6,33 +6,37 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/22 14:53:13 by amalsago          #+#    #+#             */
-/*   Updated: 2020/02/22 15:37:40 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/02/23 14:05:53 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "checker.h"
 
-static void		rotate_down(int *stack, int len)
+/*
+** rotate_down() - shift down all elements of given stack
+*/
+
+static void		rotate_down(int *stack, int top_index)
 {
 	int		i;
-	int		last;
+	int		low;
 
-	i = len + 1;
-	last = stack[len];
-	while (--i >= 0)
-		stack[i] = stack[i - 1];
-	stack[0] = last;
+	i = -1;
+	low = stack[0];
+	while (++i < top_index)
+		stack[i] = stack[i + 1];
+	stack[top_index] = low;
 }
 
 void	rra(void)
 {
-	rotate_down(get_stack()->stack_a, get_stack()->len_a);
+	rotate_down(get_stack()->stack_a, get_stack()->top_index_a);
 }
 
 void	rrb(void)
 {
-	rotate_down(get_stack()->stack_b, get_stack()->len_b);
+	rotate_down(get_stack()->stack_b, get_stack()->top_index_b);
 }
 
 void	rrr(void)
