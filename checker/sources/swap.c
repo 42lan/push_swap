13b01abb308441040827324ec041c05f44863983
4slan <amalsago@student.42.fr>
2020-02-23 14:21:59 +0100

    improve: correct behaviour of swap function

--- checker/sources/swap.c
+++ checker/sources/swap.c
@@ -6,37 +6,40 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/22 01:52:32 by amalsago          #+#    #+#             */
-/*   Updated: 2020/02/22 17:53:48 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/02/23 12:21:33 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "checker.h"
 
 /*
-** swap_top2el() - swaps two first elements of given array
+** swap_top2el() - swaps two first elements of given array pointed by
+** `top_index` and `top_index - 1`.
+** If `top_index` is less than 1, means that stack has only one element, hence
+** there's no need to swap.
 */
 
-static void		swap_top2el(int *stack, int len)
+static void		swap_top2el(int *stack, int top_index)
 {
 	int			*a;
 	int			*b;
 
-	if (len > 0)
+	if (top_index > 0)
 	{
-		a = &(stack[0]);
-		b = &(stack[1]);
+		a = &(stack[top_index]);
+		b = &(stack[top_index - 1]);
 		ft_swap_xor(a, b);
 	}
 }
 
 void			sa(void)
 {
-	swap_top2el(get_stack()->stack_a, get_stack()->len_a);
+	swap_top2el(get_stack()->stack_a, get_stack()->top_index_a);
 }
 
 void			sb(void)
 {
-	swap_top2el(get_stack()->stack_b, get_stack()->len_b);
+	swap_top2el(get_stack()->stack_b, get_stack()->top_index_b);
 }
 
 void			ss(void)
