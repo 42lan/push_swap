dc76f8b81fc3e9e1c09ba9deb11b5f0abd637642
4slan <amalsago@student.42.fr>
2020-02-23 17:49:16 +0100

    feat: count and print moves for sorting

--- checker/sources/checker.c
+++ checker/sources/checker.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/20 11:52:46 by amalsago          #+#    #+#             */
-/*   Updated: 2020/02/23 14:35:43 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/02/23 17:45:51 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,9 @@
 int			main(int ac, char **av)
 {
 	char	*line;
+	int		moves;
 
+	moves = 0;
 	if (ac < 2 || !**(++av))
 		exit(EXIT_FAILURE);
 	parse_args(ac, &av);
@@ -37,7 +39,10 @@ int			main(int ac, char **av)
 		if (!*line)
 			break;
 		if (valid_operation(line))
+		{
 			execute_operation(line);
+			++moves;
+		}
 		else
 		{
 			ft_printf("Invalid operation: [%s]\n", line);// REMOVE
@@ -50,5 +55,6 @@ int			main(int ac, char **av)
 		print_stack(get_stack()->stack_b, get_stack()->top_index_b);
 	}
 	check_sort();
+	print_sorted(moves);
 	return (0);
 }
