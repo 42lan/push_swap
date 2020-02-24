778def005e4e57bbafcd6fe01893f07bb22282b0
4slan <amalsago@student.42.fr>
2020-02-24 19:23:24 +0100

    fix: remove call to check_sort() and other print functions

--- push_swap/sources/main.c
+++ push_swap/sources/main.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/20 11:52:46 by amalsago          #+#    #+#             */
-/*   Updated: 2020/02/24 18:18:54 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/02/24 19:19:37 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,6 @@ int			main(int ac, char **av)
 		exit(EXIT_FAILURE);
 	parse_args(ac, &av);
 	initialize_stack(av);
-	//print_stacks_state();
 	sort_stack();
-	check_sort();
-	//print_sorted(moves);
 	return (0);
 }
