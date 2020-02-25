169e2a77b022c0a68ce7e4bfa9a3a55353b76a07
4slan <amalsago@student.42.fr>
2020-02-25 12:07:27 +0100

    feat: add two_sort()

--- push_swap/sources/sort_stack.c
+++ push_swap/sources/sort_stack.c
@@ -6,11 +6,20 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/24 12:39:08 by amalsago          #+#    #+#             */
-/*   Updated: 2020/02/24 18:09:02 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/02/24 19:46:56 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+void			two_sort(void)
+{
+	int			*s;
+
+	s = get_stack()->stack_a;
+	if (s[1] > s[0])
+		sa();
+}
 
 void			three_sort(void)
 {
@@ -40,6 +49,8 @@ void			sort_stack(void)
 	unsigned	size;
 
 	size = get_stack()->top_index_a + 1;
+	if (size == 2)
+		two_sort();
 	if (size == 3)
 		three_sort();
 }
