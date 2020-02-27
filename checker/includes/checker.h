bcf7c5bfd18084c3406dabd1f1cad66648c2b9a6
4slan <amalsago@student.42.fr>
2020-02-27 13:18:00 +0100

    style: place include ft_printf near other headers

--- checker/includes/checker.h
+++ checker/includes/checker.h
@@ -6,17 +6,15 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/19 17:03:15 by amalsago          #+#    #+#             */
-/*   Updated: 2020/02/23 17:46:09 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/02/27 13:15:53 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-						// REMOVE
-# include "ft_printf.h" // REMOVE
-						// REMOVE
 #ifndef CHECKER_H
 # define CHECKER_H
 
 # include "libft.h"
+# include "ft_printf.h"
 # include "get_next_line.h"
 # include "system_libraries.h"
 
@@ -64,7 +62,6 @@ void		rr(void);
 void		rra(void);
 void		rrb(void);
 void		rrr(void);
-
 
 void		check_sort(void);
 
