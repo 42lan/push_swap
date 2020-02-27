d734f7d904c1c318457cf4a95414e014dcc14dc7
4slan <amalsago@student.42.fr>
2020-02-27 11:13:04 +0100

    refact: call to repead_op()

--- push_swap/sources/four_sort.c
+++ push_swap/sources/four_sort.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/27 10:12:34 by amalsago          #+#    #+#             */
-/*   Updated: 2020/02/27 10:18:00 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/02/27 10:44:33 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,14 +27,10 @@ void			four_sort(int *s_a, int *s_b)
 			ra();
 			return ;
 		}
-		while (s_b[0] > s_a[2])
-		{
+		while (s_b[0] > s_a[2] && ++i)
 			ra();
-			++i;
-		}
 		pa();
-		while (--i >= 0)
-			rra();
+		repeat_op(rra, i);
 	}
 	else
 		pa();
