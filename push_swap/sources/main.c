d7fca2aa4e6caff41b3e4cdf6ee52f35fef83b92
4slan <amalsago@student.42.fr>
2020-03-06 09:10:59 +0100

    feat: add function to free stack

--- push_swap/sources/main.c
+++ push_swap/sources/main.c
@@ -6,26 +6,25 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/20 11:52:46 by amalsago          #+#    #+#             */
-/*   Updated: 2020/03/01 16:59:12 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/03/06 09:10:10 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int		main(int ac, char **av)
+static void		free_stack(t_stack *stack)
+{
+	free(stack->a);
+	free(stack->b);
+}
+
+int				main(int ac, char **av)
 {
 	if (ac < 2 || !**(++av))
 		exit(EXIT_FAILURE);
 	parse_args(ac, &av);
 	initialize_stack(av);
-	if (stack()->tia == 0 && stack()->tib == -1)
-		ft_printf("Only one elements in STACK A and nothin in STACK B\n");
-	else if (check_sort())
-		ft_printf("Stack A is sorted\n");
-	else
-	{
-		sort_stack();
-		check_sort(); // REMOOOOVE
-	}
+	sort_stack();
+	free_stack(stack());
 	return (0);
 }
