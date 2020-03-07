efbcb746fe132aa38a6319ad5fd9108369e76282
4slan <amalsago@student.42.fr>
2020-03-07 23:27:58 +0100

    readd system_libraries.h

---
+++ includes/system_libraries.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   system_libraries.h                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/19 16:47:56 by amalsago          #+#    #+#             */
+/*   Updated: 2020/03/06 21:12:43 by amalsago         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SYSTEM_LIBRARIES_H
+# define SYSTEM_LIBRARIES_H
+
+# include <stdlib.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+
+/*
+** SYSTEM_LIBRARIES_H
+*/
+#endif
