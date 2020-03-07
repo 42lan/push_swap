641120aec3fcc972568c03c166cc2eb3080431a9
4slan <amalsago@student.42.fr>
2020-03-07 23:30:08 +0100

    test: correcting behaviour of average_op.sh

--- average_op.sh
+++ average_op.sh
@@ -6,7 +6,7 @@
 #    By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/03/05 16:34:25 by amalsago          #+#    #+#              #
-#    Updated: 2020/03/05 23:10:19 by amalsago         ###   ########.fr        #
+#    Updated: 2020/03/07 23:29:36 by amalsago         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -15,7 +15,7 @@ PUSH_SWAP=./push_swap
 # How many times repeating a test to get average
 NTIMES=1
 # Array of ranges
-declare -a array=( 2 3 4 5 6 7 8 9 10 25 50 100 500 1000 ) 
+declare -a array=( 2 3 4 5 6 7 8 9 10 25 50 100 500 900 1000 )
 # Length of array
 length=${#array[@]}
 # Repeat the while loop for each field of array
@@ -26,15 +26,13 @@ do
 	SUM=0
 	FROM=1
 	TO=${array[$i]}
-	printf "[%d %5d] " $FROM $TO
-	while [ $I -le $NTIMES ]; do
+	#printf "[%d %5d] " $FROM $TO
+	while [ $I -lt $NTIMES ]; do
 		ARG=$(ruby -e "puts ($FROM..$TO).to_a.shuffle.join(' ')");
 		NOP=$($PUSH_SWAP $ARG | wc | awk '{ print $1 }');
 		SUM=$(expr $SUM + $NOP)
 		I=$(expr $I + 1)
-		if [ $I -gt $NTIMES ]; then
-			AVERAGE=$(expr $SUM / $NTIMES)
-			printf "%-d\n" $AVERAGE
-		fi
 	done
+	AVERAGE=$(expr $SUM / $NTIMES)
+	printf "%-d\n" $AVERAGE
 done
