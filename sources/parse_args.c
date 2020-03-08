c5f72bf4de55174ea7d45f42cbdc937c53f98feb
4slan <amalsago@student.42.fr>
2020-03-08 09:05:06 +0100

    fix: release memory when it was alloc with prepare_args

--- sources/parse_args.c
+++ sources/parse_args.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/20 12:02:03 by amalsago          #+#    #+#             */
-/*   Updated: 2020/03/07 23:17:42 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/03/08 00:15:41 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,5 +40,8 @@ void			parse_args(int ac, char ***p_av)
 {
 	(ac == 2) ? *p_av = prepare_args(*p_av) : 0;
 	if (check_ints(*p_av) || check_dups(*p_av) || check_lims(*p_av))
+	{
+		(ac == 2) ? ft_memdel((void **)*p_av) : 0;
 		ft_perror_exit("Error");
+	}
 }
