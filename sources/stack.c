fe1d644803d4bba12b72390c04945bb18fbb3650
4slan <amalsago@student.42.fr>
2020-03-09 08:39:02 +0100

    fix(MAJOR): fix malloc error which happens randomly
    Allocating `sizeof(int) * top_index` caused eror "malloc: Incorrect checksum
    for freed object". Program was trying to write in a not allocated zone.
    Thanks to @ataguiro

--- sources/stack.c
+++ sources/stack.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/22 01:59:39 by amalsago          #+#    #+#             */
-/*   Updated: 2020/03/08 00:08:39 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/03/09 08:23:08 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,22 +25,24 @@ void				free_stack(void)
 	ft_memdel((void **)&stack()->b);
 }
 
-void				initialize_stack(char **av)
+int					initialize_stack(int ac, char **av)
 {
-	int				top_index;
+	int				size;
 
-	top_index = ft_arraysize(av) - 1;
+	size = ft_arraysize(av);
 	if (!(stack()->a = ft_ca2ia(av)))
 	{
 		(ERROR_MANAGEMENT) ? ft_putendl(MALLOC_FAILURE) : 0;
-		exit(EXIT_FAILURE);
+		return (FAILURE);
 	}
-	if(!(stack()->b = (int *)ft_memalloc(sizeof(int) * top_index)))
+	if (!(stack()->b = (int *)ft_memalloc(sizeof(int) * size)))
 	{
 		ft_memdel((void **)&stack()->a);
 		(ERROR_MANAGEMENT) ? ft_putendl(MALLOC_FAILURE) : 0;
-		exit(EXIT_FAILURE);
+		return (FAILURE);
 	}
-	stack()->tia = top_index;
+	stack()->tia = size - 1;
 	stack()->tib = -1;
+	(ac == 1) ? ft_memdel((void **)av) : 0;
+	return (SUCCESS);
 }
