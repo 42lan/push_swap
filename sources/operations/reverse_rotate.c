e919a6a7860719c655e2fa52988e143c7f36528d
4slan <amalsago@student.42.fr>
2020-03-10 20:55:22 +0100

    fix(MAJOR): check to avoid heap-buffer-overflow
    Executing rotate operation on an empty stack cause heap-buffer-overflow
    as it try to access and write on a non allocated zone.

--- sources/operations/reverse_rotate.c
+++ sources/operations/reverse_rotate.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/22 14:53:13 by amalsago          #+#    #+#             */
-/*   Updated: 2020/03/09 08:21:32 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/03/10 20:44:48 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,13 @@ static void		rotate_down(int *stack, int top_index)
 	int			low;
 
 	i = -1;
-	low = stack[0];
-	while (++i < top_index)
-		stack[i] = stack[i + 1];
-	stack[top_index] = low;
+	if (top_index > 0)
+	{
+		low = stack[0];
+		while (++i < top_index)
+			stack[i] = stack[i + 1];
+		stack[top_index] = low;
+	}
 }
 
 void			rra(int print_op)
--- sources/operations/rotate.c
+++ sources/operations/rotate.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/22 14:24:01 by amalsago          #+#    #+#             */
-/*   Updated: 2020/03/09 08:21:17 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/03/10 20:45:04 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,13 @@ static void		rotate_up(int *stack, int top_index)
 {
 	int			top;
 
-	top = stack[top_index];
-	while (--top_index >= 0)
-		stack[top_index + 1] = stack[top_index];
-	stack[0] = top;
+	if (top_index > 0)
+	{
+		top = stack[top_index];
+		while (--top_index >= 0)
+			stack[top_index + 1] = stack[top_index];
+		stack[0] = top;
+	}
 }
 
 void			ra(int print_op)
